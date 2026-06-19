c3bb50ceb3e8dc9b1c6667186449c8d5b0712739
Ion Cerezo <ioncerezov@gmail.com>
2026-06-19 11:24:36 +0200

    Contenedor de angular

--- app-amistad-angular/README.md
+++ app-amistad-angular/README.md
@@ -1,4 +1,4 @@
-# AppAmistad
+# AppAmistadAngular
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 22.0.1.
 
--- app-amistad-angular/src/app/app.ts
+++ app-amistad-angular/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('app-amistad');
+  protected readonly title = signal('app-amistad-angular');
 }
