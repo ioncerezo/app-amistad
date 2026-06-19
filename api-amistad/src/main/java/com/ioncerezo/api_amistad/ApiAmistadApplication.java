832952a74b27df41a34988b59b72ff6eddbbc01a
Ion Cerezo <ioncerezov@gmail.com>
2026-06-19 11:49:34 +0200

    crear proyecto spring base

---
+++ api-amistad/src/main/java/com/ioncerezo/api_amistad/ApiAmistadApplication.java
@@ -0,0 +1,13 @@
+package com.ioncerezo.api_amistad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAmistadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAmistadApplication.class, args);
+	}
+
+}
---
+++ api-amistad/src/test/java/com/ioncerezo/api_amistad/ApiAmistadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioncerezo.api_amistad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAmistadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
